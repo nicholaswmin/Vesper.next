a6c6b1bb1cc66c530ff8125a333a1e2ec5a5494d
nicholaswmin <nicholas.kyriakides@my.westminster.ac.uk>
2014-02-23 14:19:27 +0200

    Input cases
    Added numeric.js (restricts inputs to numbers and dot/decimal points)
    
    Take care of invalid "0" value in input
    
    Renamed function to hideRightTools
    
    Enter/OnChange events in inputs trigger respective functions
    
    "Select All" triggered on input click

--- v.next/js/ext-editorNext.js
+++ v.next/js/ext-editorNext.js
@@ -89,11 +89,26 @@ function setElementTypeRasterEngrave() {
 function setElementHeight() {
 	clipboard = captureSelectionState();
 	var selected = paper.project.selectedItems;
-	for (var i = 0; i < selected.length; i++) {
-	var currentHeight = selected[i].bounds["height"];
 	var newHeight = document.getElementById('elementHeight').value;
+
+	//Do an if/else here to detect if "0" is passed as the new height. If that's true then set the element height to 1 because ''0'' is actually an invalid value
+	for (var i = 0; i < selected.length; i++) {
+		 if (newHeight==0) {
+	document.getElementById("elementHeight").value =1;//Also set the input text to ''1'
+	var currentHeight = selected[i].bounds["height"];
+	var newHeight = 1;
 	var scaleCoefficient = newHeight/currentHeight;
 	selected[i].scale(1,scaleCoefficient);
+	 	
+	 }
+	     else {
+
+	var currentHeight = selected[i].bounds["height"];
+	var newHeight = document.getElementById('elementHeight').value;
+	console.log(currentHeight);
+	var scaleCoefficient = newHeight/currentHeight;
+	selected[i].scale(1,scaleCoefficient);
+}
 	}
 	undo.snapshot("Cut");
 }
@@ -104,11 +119,25 @@ function setElementHeight() {
 function setElementWidth() {
 	clipboard = captureSelectionState();
 	var selected = paper.project.selectedItems;
+	var newWidth = document.getElementById('elementWidth').value;
+	//Do an if/else here to detect if "0" is passed as the new width. If that's true then set the element width to 1 because ''0'' is actually an invalid value
 	for (var i = 0; i < selected.length; i++) {
+		 if (newWidth==0) {
+	document.getElementById("elementWidth").value =1; //Also set the input text to ''1''
+	var currentWidth = selected[i].bounds["width"];
+	var newWidth = 1;
+	var scaleCoefficient = newWidth/currentWidth;
+	selected[i].scale(scaleCoefficient,1);
+	 	
+	 }
+	     else {
+
+
 	var currentWidth = selected[i].bounds["width"];
 	var newWidth = document.getElementById('elementWidth').value;
 	var scaleCoefficient = newWidth/currentWidth;
 	selected[i].scale(scaleCoefficient,1);
+}
 	}
 	undo.snapshot("Cut");
 }
@@ -125,7 +154,7 @@ function setElementYPosition() {
 
 //Function that hides the right tools when nothing is selected. Triggered by clicking on the canvas.
 
-function resetInputFields(){
+function hideRightTools(){
 	var selected = paper.project.selectedItems;
 	if (selected.length<1){
 		$('#tools_right').hide();
@@ -133,21 +162,16 @@ function resetInputFields(){
 	}
 }
 
-//Function that prevents the user from typing anything ELSE than NUMBERS in the x/y width/height input fields in the HTML file.
+//Function that prevents the user from typing anything ELSE than NUMBERS in the x/y width/height input fields in the HTML file. Uses ''numeric.js'' file.
 
 $(document).ready(function() {
 
 
-$('#elementXPosition,#elementYPosition,#elementWidth,#elementHeight').keypress(function(e) {
-    var a = [];
-    var k = e.which;
 
-    for (i = 48; i < 58; i++)
-        a.push(i);
-
-    if (!(a.indexOf(k)>=0))
-        e.preventDefault();
+$("#elementXPosition,#elementYPosition,#elementWidth,#elementHeight").numeric();
 });
+
+$(document).ready(function() {
 
 //The following path is added at startup of the editor. It's supposed to represent the material size selected by the user
 //which is used for showing the users where to concentrate his elements. The boolOp/3D algorithms use this for the cutting rectangle.
@@ -185,7 +209,8 @@ $("#tool-material").click(function() {
 		bringToFrontSelection();
 	});
 
-//The following functions are triggered on an ''onChange'' event. 
+//The following functions are triggered on an ''onChange'' event. The same functions are triggered when pressing ''ENTER'' key while typing. See section below this
+//snippet
 	$("#elementHeight").change(function() {
 		setElementHeight();
 	});
@@ -203,7 +228,35 @@ $("#tool-material").click(function() {
 	});
 
 			$("#canvas").click(function() {
-				resetInputFields();
+				hideRightTools();
 
 	});
+
+//Here the previous functions are also triggered on pressing ''ENTER'' key while typing. Keycode 13 is the ''ENTER'' key
+document.getElementById('elementHeight').onkeydown = function(event) {
+    if (event.keyCode == 13) {
+      setElementHeight();
+    }
+};
+
+document.getElementById('elementWidth').onkeydown = function(event) {
+    if (event.keyCode == 13) {
+      setElementWidth();
+    }
+};
+document.getElementById('elementYPosition').onkeydown = function(event) {
+    if (event.keyCode == 13) {
+      setElementYPosition();
+    }
+};
+document.getElementById('elementXPosition').onkeydown = function(event) {
+    if (event.keyCode == 13) {
+      setElementXPosition();
+    }
+};
+
+$('#elementXPosition,#elementYPosition,#elementWidth,#elementHeight').click(function () {
+    this.select();
+});
+
 });
